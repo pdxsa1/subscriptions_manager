1db6a3c27b34fa09d3019818c4b15633883af1f8
Sal Ambriz <salambriz@Sals-MacBook-Pro.local>
2026-03-27 01:47:46 -0700

    added user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,9 @@ import {Router} from 'express'
 const userRouter = Router();
 
 userRouter.get('/', (req,res) => res.send({title: 'GET all users'}));
-userRouter.get('/:id', (req,res) => res.send({title: 'GET all users'}));
-userRouter.get('/', (req,res) => res.send({title: 'GET all users'}));
-userRouter.get('/', (req,res) => res.send({title: 'GET all users'}));
-userRouter.get('/', (req,res) => res.send({title: 'GET all users'}));
+userRouter.get('/:id', (req,res) => res.send({title: 'GET user details'}));
+userRouter.get('/', (req,res) => res.send({title: 'CREATE new user'}));
+userRouter.get('/:id', (req,res) => res.send({title: 'UPDATE user'}));
+userRouter.get('/:id', (req,res) => res.send({title: 'DELETE user'}));
+
+export default userRouter;
